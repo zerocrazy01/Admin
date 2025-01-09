cf6037afc11b32881230cd5153ba47d4d7b95294
Naswan100 <naswan.tpg.10@gmail.com>
2025-01-09 19:08:33 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,5 @@
 <?php
 session_start();
-
-// Redirect jika pengguna sudah login
-if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
-    header("Location: index.php");
-    exit;
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,6 +11,50 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap">
+    <style>
+        body {
+            background: linear-gradient(to right, #6a11cb, #2575fc);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: 'Roboto', sans-serif;
+        }
+        .login-box {
+            width: 400px;
+            padding: 30px;
+            background-color: #fff;
+            border-radius: 15px;
+            box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.3);
+            text-align: center;
+        }
+        .login-logo b {
+            color: #2575fc;
+        }
+        .login-box-body {
+            margin-top: 20px;
+        }
+        .form-group input {
+            border-radius: 25px;
+        }
+        .btn {
+            border-radius: 25px;
+            transition: 0.3s;
+        }
+        .btn:hover {
+            background-color: #0056b3;
+        }
+        .login-box a {
+            color: #2575fc;
+            font-weight: 500;
+            transition: 0.3s;
+        }
+        .login-box a:hover {
+            text-decoration: underline;
+        }
+    </style>
 </head>
 <body>
     <div class="login-box">
@@ -25,22 +63,27 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
         </div>
         <div class="login-box-body">
             <p class="login-box-msg">Sign in to start your session</p>
+            <!-- Form login -->
             <form action="proses_login.php" method="post">
+                <!-- Input username -->
                 <div class="form-group has-feedback">
-                    <input type="email" name="email" class="form-control" placeholder="Email" required>
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    <input type="text" name="username" class="form-control" placeholder="Username" required>
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
                 </div>
+                <!-- Input password -->
                 <div class="form-group has-feedback">
                     <input type="password" name="password" class="form-control" placeholder="Password" required>
                     <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                 </div>
+                <!-- Tombol login -->
                 <div class="row">
-                    <div class="col-xs-4">
+                    <div class="col-xs-12">
                         <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
                     </div>
                 </div>
             </form>
-            <a href="register.html" class="text-center">Register a new membership</a>
+            <!-- Link ke halaman registrasi -->
+            <a href="register.php" class="text-center" style="display: block; margin-top: 15px;">Register a new membership</a>
         </div>
     </div>
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
