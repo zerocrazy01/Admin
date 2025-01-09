94f67308096f5c2651679280fdb0320215b862be
Naswan100 <naswan.tpg.10@gmail.com>
2025-01-09 19:10:25 +0800

    Update siswa.php

--- siswa/siswa.php
+++ siswa/siswa.php
@@ -1,71 +1,44 @@
-
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Data Siswa
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Siswa</a></li>
-      </ol>
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-                    
-          <!-- /.box -->
-
-          <div class="box">
-            <div class="box-header">
-              <a href="index.php?hal=tambah_siswa">
-              <input type="button" value="Tambah" class="btn btn-primary" name="">
-              </a>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <table id="example1" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th>No</th>
-                  <th>Nama</th>
-                  <th>Harga</th>
-                  <th>Stok</th>
-                  <th>Aksi</th>
-                </tr>
-                </thead>
-                <tbody>
 <?php
-include "koneksi.php";
-$hasil=mysqli_query($mysqli,"select * from minuman");
-$no=1;
-while($row=mysqli_fetch_array($hasil)){
+include_once "koneksi.php";
+
+// Periksa apakah koneksi berhasil
+if (!$mysqli) {
+    die("Koneksi gagal: " . $mysqli->connect_error);
+}
+
+// Jalankan query untuk mengambil data dari tabel 'minuman'
+$query = "SELECT * FROM minuman";
+$hasil = $mysqli->query($query);
+
+// Periksa apakah query berhasil
+if (!$hasil) {
+    die("Query gagal: " . $mysqli->error);
+}
+
+// Inisialisasi nomor urut
+$no = 1;
+
+// Iterasi melalui hasil query dan tampilkan data
+while ($row = $hasil->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>" . $no++ . "</td>";
+    echo "<td>" . htmlspecialchars($row['nama'], ENT_QUOTES, 'UTF-8') . "</td>";
+    echo "<td>" . htmlspecialchars($row['harga'], ENT_QUOTES, 'UTF-8') . "</td>";
+    echo "<td>" . htmlspecialchars($row['stok'], ENT_QUOTES, 'UTF-8') . "</td>";
+    echo "<td>";
+    echo "<a href='index.php?hal=edit_siswa&id=" . htmlspecialchars($row['Id'], ENT_QUOTES, 'UTF-8') . "'>";
+    echo "<button type='button' class='btn btn-warning'><i class='fa fa-pencil'></i> Edit</button>";
+    echo "</a> ";
+    echo "<a onclick='return confirm(\"Anda Yakin...?\")' href='siswa/hapus_siswa.php?id=" . htmlspecialchars($row['Id'], ENT_QUOTES, 'UTF-8') . "'>";
+    echo "<button type='button' class='btn btn-danger'><i class='fa fa-trash'></i> Hapus</button>";
+    echo "</a>";
+    echo "</td>";
+    echo "</tr>";
+}
+
+// Tutup hasil query
+$hasil->free();
+
+// Tutup koneksi database
+$mysqli->close();
 ?>
-                <tr>
-                  <td><?php echo $no; $no++;?></td>
-                  <td><?php echo $row['nama']?></td>
-                  <td><?php echo $row['harga']?></td>
-                  <td><?php echo $row['stok']?></td>
-                  
-                  
-                  <td><a href="index.php?hal=edit_siswa&id=<?php echo $row['Id']?>"><button type="button" class="btn btn-warning" name=""> <i class="fa fa-pencil"></i> Edit</button></a>
-                    <a onclick="return confirm('Anda Yakin...?')" href="siswa/hapus_siswa.php?id=<?php echo $row['Id']?>">
-                    <button type="button" class="btn btn-danger" name=""> <i class="fa fa-trash"></i> Hapus</button></a>
-                  </td>
-                </tr>
-<?php } ?>
-                </tfoot>
-              </table>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-  </div>
